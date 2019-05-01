7e1cd97f8d6d3ddd1094193c420dc9bf3b3d4726
lyhoanam <lyhoanam@gmail.com>
2019-05-02 01:03:24 +0700

    add some filed

--- endpoint/model/product.py
+++ endpoint/model/product.py
@@ -3,13 +3,26 @@ db = SQLAlchemy()
 
 
 class Product(db.Model):
-    __tablename__ = 'product'
+    __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
-
+    title = db.Column(db.String(200))
+    description = db.Column(db.String(400))
+    img_url = db.Column(db.String(200))
+    regular_price = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
     def __init__(self, name=None):
         self.name = name
 
     def __repr__(self):
-        return 'Id: {}, name: {}'.format(self.id, self.name)
+        return """Id: {}, 
+        name: {},
+        title: {},
+        description: {},
+        img_url: {},
+        regular_price: {},
+        quantity: {}
+        """.format(self.id, self.name, self.title, 
+        self.description, self.img_url, 
+        self.regular_price, self.quantity)
