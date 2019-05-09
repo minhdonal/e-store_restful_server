ca8f26bff0429f16b6f4da3e9a1fa3b0cf6074e0
Minh DN <namminhlp@gmail.com>
2019-05-09 21:24:13 +0700

    [REF] pep8 and conflict

--- endpoint/model/product.py
+++ endpoint/model/product.py
@@ -3,7 +3,7 @@ db = SQLAlchemy()
 
 
 class Product(db.Model):
-    __tablename__ = 'products'
+    __tablename__ = 'product'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
@@ -14,19 +14,22 @@ class Product(db.Model):
     regular_price = db.Column(db.Integer)
     quantity = db.Column(db.Integer)
     img_url = db.Column(db.String(255))
+
     def __init__(self, name=None):
         self.name = name
 
     def __repr__(self):
         if product_status_id != 1:
             return False
-        return """Id: {}, 
-        name: {},
-        title: {},
-        description: {},
-        img_url: {},
-        regular_price: {},
-        quantity: {}
+        
+        return """
+            Id: {}, 
+            name: {},
+            title: {},
+            description: {},
+            img_url: {},
+            regular_price: {},
+            quantity: {}
         """.format(self.id, self.name, self.title, 
         self.description, self.img_url, 
         self.regular_price, self.quantity)
